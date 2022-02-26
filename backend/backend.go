c0e6fbb2016b9be06b4df1440705e3b3e788db24
Joe Luhrman <jaluhrman2018@gmail.com>
2022-02-25 21:30:12 -0800

    added structs and constants

--- backend/backend.go
+++ backend/backend.go
@@ -1,1 +1,27 @@
 package backend
+
+type MuscleGroup string
+
+const (
+	Legs MuscleGroup = "Legs"
+	Push MuscleGroup = "Push"
+	Pull MuscleGroup = "Pull"
+)
+
+type ExerciseType struct {
+	Name   string
+	MscGrp MuscleGroup
+}
+
+type ExerciseEntry struct {
+	Type  ExerciseType
+	Sets  int
+	Reps  int
+	Notes string
+}
+
+type WorkoutEntry struct {
+	Date      string
+	Exercises []ExerciseEntry
+	Notes     string
+}
