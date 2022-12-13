9ad40eb15adb09b2e4a931fc4c8fa785e9d3e79d
Joe Luhrman <jaluhrman2018@gmail.com>
2022-12-12 20:21:37 -0800

    updated_at added

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -1,15 +1,35 @@
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE IF NOT EXISTS users (
     id          SERIAL PRIMARY KEY,
     username    TEXT UNIQUE NOT NULL,
     hashed_password    TEXT NOT NULL,
     is_admin    BOOLEAN,
 
-    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
     user_id     INTEGER PRIMARY KEY,
     token       TEXT UNIQUE,
 
-    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON sessions
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
