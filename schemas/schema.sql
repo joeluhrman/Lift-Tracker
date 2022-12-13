a9992a2ea6e76635c162c2be07681824e8a9a339
Joe Luhrman <jaluhrman2018@gmail.com>
2022-12-12 20:16:12 -0800

    created_at working

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -2,10 +2,14 @@ CREATE TABLE IF NOT EXISTS users (
     id          SERIAL PRIMARY KEY,
     username    TEXT UNIQUE NOT NULL,
     hashed_password    TEXT NOT NULL,
-    is_admin    BOOLEAN
+    is_admin    BOOLEAN,
+
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
     user_id     INTEGER PRIMARY KEY,
-    token       TEXT UNIQUE
+    token       TEXT UNIQUE,
+
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
--- todo.md
+++ todo.md
@@ -1,7 +1,4 @@
 # ToDo
 
 1. Change testing db to localhost for development (this is much harder than anticipated)
-2. Session management/expiration
-    a. idea -> give no cookie expiration so automatically expires when client shuts down.
-        Keep track of client connection somehow and delete session row from db when it does.
 3. Get metadata for tables working
