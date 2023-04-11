1a6ff38a14faff9053ff14e50e12c8ebf01c0b40
Joe Luhrman <jaluhrman2018@gmail.com>
2023-04-10 22:47:13 -0800

    who knows

--- web/src/components/NavHeader.jsx
+++ web/src/components/NavHeader.jsx
@@ -1,17 +1,30 @@
+import React from "react"
 import {
     Button,
     Container,
     Nav,
     Navbar,
 } from "react-bootstrap"
+import {
+  Navigate
+} from "react-router-dom"
 import logoUrl from "../assets/lt.png"
 import UserHandler from "../handlers/UserHandler"
 const userHandler = new UserHandler()
 
 export default function NavHeader() {
+  const [toLogin, setToLogin] = React.useState(false)
+
+  const handleLogout = async() => {
+    const res = await userHandler.logout()
+    setToLogin(true)
+  }
+
+  if (toLogin) return <Navigate to="/login"/>
+
   return (
     <Navbar bg="light" expand="lg" className="border-bottom" fixed="top">
-      <Container>
+      <Container fluid>
         <Navbar.Brand href="/">
           <img style={{paddingRight: "5%"}}
             src={logoUrl}
@@ -24,8 +37,18 @@ export default function NavHeader() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="container-fluid">
+            <Nav.Item>
+              <Nav.Link href="#">
+                Workout History
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/workout-templates">
+                Workout Templates
+              </Nav.Link>
+            </Nav.Item>
             <Nav.Item className="ms-auto">
-              <Button onClick={userHandler.logout}>Logout</Button>
+              <Button onClick={handleLogout}>Logout</Button>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
