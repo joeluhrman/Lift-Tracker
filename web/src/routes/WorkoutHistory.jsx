a38e5a60b85cac38bae6f255c230087e401c0287
Joe Luhrman <jaluhrman2018@gmail.com>
2023-05-06 18:30:07 -0400

    added button to navigate to form

--- web/src/routes/WorkoutHistory.jsx
+++ web/src/routes/WorkoutHistory.jsx
@@ -1,3 +1,18 @@
+import { useNavigate } from "react-router-dom"
+import { Button, Container } from "react-bootstrap"
+import CRUDWorkout from "./CRUDWorkout"
+
 export default function WorkoutHistory() {
-    return (<h2>History</h2>)
+    const navigate = useNavigate()
+
+    const handleToAddWL = () => navigate("/add-workout-log")
+
+    return (<>
+        <h2>History</h2>
+        <Container fluid className="d-flex mb-2">
+            <Button variant="outline-primary" className="ms-auto" onClick={handleToAddWL}>
+                Log Workout
+            </Button>
+        </Container>
+    </>)
 }
